c73cc2577746a2687c2b91b7597dacc7013d6868
BudgieInWA <benjaminaritter@gmail.com>
2014-10-20 17:25:40 +0800

    Added sensor test data.

--- init-db-testdata.sh
+++ init-db-testdata.sh
@@ -1,4 +1,5 @@
 
 sqlite3 preferences.sqlite < preferences-test.sql && echo "Preferences test data added."
+sqlite3 sensor.sqlite < sensor-test.sql && echo "Sensor test data added."
 
 echo "All done adding test data :)"
