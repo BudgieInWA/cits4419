25380cf8ebb70b59b5b77bb6e86caa744d30c3f5
BudgieInWA <benjaminaritter@gmail.com>
2014-10-22 19:45:30 +0800

    Implemented site and POST logic

--- server/server.py
+++ server/server.py
@@ -3,6 +3,7 @@ recognition automation and a REASTful API for reading the detection sensor."""
 
 import flask
 import sqlite3
+import re
 
 API_BASE = "/apiv0/"
 
@@ -22,12 +23,97 @@ def get_database(db):
 app = flask.Flask(__name__)
 
 
+pattern = re.compile("pts\[([a-z]+)\]\[([0-9]+)\]")
+def save_prefs(prefs, data):
+    for k in data:
+        res = pattern.match(k) 
+        if res:
+            p = res.group(1)
+            s = int(res.group(2))
+
+            if data[k] == 'notset':
+                prefs.execute("DELETE FROM person_to_switch WHERE person=? AND switch=?",
+                        (p, s))
+            else:
+                if data[k] == 'on': v = 1
+                elif data[k] == 'off': v = 0
+                else: continue;
+                prefs.execute("REPLACE INTO person_to_switch VALUES(?,?,?)",
+                        (p,s,v))
+
 # Website routes.
 @app.route("/")
 def hello_world():
     people = get_database("prefs").execute("SELECT * FROM people");
     return "Hello World!<br/>" + "<br/>".join(p["name"] for p in people)
 
+@app.route("/people", methods=["GET"])
+def site_list_people():
+    people = get_database("prefs").execute("SELECT * FROM people");
+    return "List of people:<br/>" + "<br/>".join(p["name"] for p in people)
+
+@app.route("/people/<string:person>", methods=["GET", "POST"])
+def site_person_settings(person):
+    prefs = get_database("prefs")
+    
+    if flask.request.method == 'POST':
+        save_prefs(prefs, flask.request.form)
+
+    people = prefs.execute("SELECT * FROM people WHERE id=?",
+            (person,));
+    p = people.fetchone()
+    if not p: abort(404, "Person %s couldn't be found"%person)
+    
+    entries = prefs.execute("SELECT * FROM person_to_switch WHERE person=?",
+            (person,))
+    vals = {}
+    for e in entries:
+        vals[e['switch']] = e['state']
+
+    out = [p['name'] + " Settings"]
+
+    switches = prefs.execute("SELECT * FROM switches")
+    for s in switches:
+        state = "not set"
+        if s['id'] in vals:
+            state = "on" if vals[s['id']] else "off"
+        out.append("switch %d is %s" % (s['id'], state))
+
+    return "<br/>".join(out)
+
+@app.route("/switches", methods=["GET"])
+def site_list_switches():
+    switches = get_database("prefs").execute("SELECT * FROM switches");
+    return "List of switches:<br/>" + "<br/>".join(s["description"] for s in switches)
+
+@app.route("/switches/<int:switch>", methods=["GET", "POST"])
+def site_switch_details(switch):
+    prefs = get_database("prefs")
+
+    if flask.request.method == 'POST':
+        save_prefs(prefs, flask.request.form)
+
+    switches = prefs.execute("SELECT * FROM switches WHERE id=?",
+            (switch,));
+    s = switches.fetchone()
+    if not s: abort(404, "Switch %s couldn't be found"%switch)
+    
+    entries = prefs.execute("SELECT * FROM person_to_switch WHERE switch=?",
+            (switch,))
+    vals = {}
+    for e in entries:
+        vals[e['person']] = e['state']
+
+    out = [s['description'] + " Settings"]
+
+    people = prefs.execute("SELECT * FROM people")
+    for p in people:
+        state = "not set"
+        if p['id'] in vals:
+            state = "on" if vals[p['id']] else "off"
+        out.append("person %s makes this %s" % (p['name'], state))
+
+    return "<br/>".join(out)
 
 # RESTful API stuff
 def dict_from_row(row):
